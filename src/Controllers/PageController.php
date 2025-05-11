99cd697c52eb2a164952d59e0c5e437bc47c7323
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-05-11 06:13:33 -0700

    added job model and job repository

--- src/Controllers/PageController.php
+++ src/Controllers/PageController.php
@@ -4,7 +4,12 @@ namespace App\Controllers;
 
 class PageController extends AbstractController {
 
+    //create a constructor that will accept the Job Repository
+    
+
     public function showHomePage() {
+        //create a repo function that will get the first 6 of the job list
+
         $this->render("home.view", []);
     }
 }
---
+++ src/Model/JobModel.php
@@ -0,0 +1,19 @@
+<?php
+
+class JobModel {
+    public int $id;
+    public int $user_id;
+    public string $title;
+    public string $description;
+    public string $salary;
+    public string $tag;
+    public string $company;
+    public string $address;
+    public string $city;
+    public string $state;
+    public string $phone;
+    public string $email;
+    public string $requirements;
+    public string $benefits;
+    public string $created_at;
+}
