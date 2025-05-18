cf5ff274e33e7159ac949d4f8f19fd4f8b5a8034
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-05-18 06:12:54 -0700

    added security helper into the showJob view

--- src/views/pages/showJob.view.php
+++ src/views/pages/showJob.view.php
@@ -19,19 +19,19 @@
       <?php endif; ?>
     </div>
     <div class="p-4">
-      <h2 class="text-xl font-semibold"><?= $job->title ?></h2>
+      <h2 class="text-xl font-semibold"><?= e($job->title); ?></h2>
       <p class="text-gray-700 text-lg mt-2">
-        <?= $job->description ?>
+        <?= e($job->description); ?>
       </p>
       <ul class="my-4 bg-gray-100 p-4">
-        <li class="mb-2"><strong>Salary:</strong><?php echo "$" . number_format($job->salary, 2); ?></li>
+        <li class="mb-2"><strong>Salary:</strong><?php echo "$" . number_format(e($job->salary), 2); ?></li>
         <li class="mb-2">
-          <strong>Location:</strong> <?= $job->city ?>, <?= $job->state ?>
+          <strong>Location:</strong> <?= e($job->city); ?>, <?= e($job->state); ?>
           <!-- <span class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span> -->
         </li>
         <?php if (!empty($job->tags)) : ?>
           <li class="mb-2">
-            <strong>Tags:</strong> <?= $job->tags ?>
+            <strong>Tags:</strong> <?= e($job->tags); ?>
           </li>
         <?php endif; ?>
       </ul>
@@ -46,16 +46,16 @@
       Job Requirements
     </h3>
     <p>
-      <?= $job->requirements ?>
+      <?= e($job->requirements); ?>
     </p>
     <h3 class="text-lg font-semibold mt-4 mb-2 text-blue-500">Benefits</h3>
-    <p><?= $job->benefits ?></p>
+    <p><?= e($job->benefits); ?></p>
   </div>
   <p class="my-5">
     Put "Job Application" as the subject of your email and attach your
     resume.
   </p>
-  <a href="mailto:<?= $job->email ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+  <a href="mailto:<?= e($job->email); ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
     Apply Now
   </a>
 </section>
