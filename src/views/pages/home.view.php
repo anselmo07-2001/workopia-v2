fd6f8999a76ca155db90c4e9d66d2b18d39d8b3f
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-05-18 06:07:57 -0700

    added security helper into the homepage

--- src/views/pages/home.view.php
+++ src/views/pages/home.view.php
@@ -23,23 +23,23 @@
         <?php foreach($jobs AS $job):?>
             <div class="rounded-lg shadow-md bg-white">
                 <div class="p-4">
-                    <h2 class="text-xl font-semibold"><?= $job->title ?></h2>
+                    <h2 class="text-xl font-semibold"><?= e($job->title); ?></h2>
                     <p class="text-gray-700 text-lg mt-2">
-                    <?= $job->description ?>
+                    <?= e($job->description); ?>
                     </p>
                     <ul class="my-4 bg-gray-100 p-4 rounded">
-                    <li class="mb-2"><strong>Salary:</strong><?php echo "$" . number_format($job->salary, 2); ?></li>
+                    <li class="mb-2"><strong>Salary:</strong><?php echo "$" . number_format(e($job->salary), 2); ?></li>
                     <li class="mb-2">
-                        <strong>Location:</strong> <?= $job->city ?>, <?= $job->state ?>
+                        <strong>Location:</strong> <?= e($job->city); ?>, <?= e($job->state); ?>
                         <!-- <span class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span> -->
                     </li>
                     <?php if (!empty($job->tags)) : ?>
                         <li class="mb-2">
-                        <strong>Tags:</strong> <?= $job->tags ?>
+                        <strong>Tags:</strong> <?= e($job->tags) ?>
                         </li>
                     <?php endif; ?>
                     </ul>
-                    <a href="/Projects/Workopia/public/index.php?path=jobs/<?= $job->id?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+                    <a href="/Projects/Workopia/public/index.php?path=jobs/<?= e($job->id); ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                         Details
                     </a>
                 </div>
